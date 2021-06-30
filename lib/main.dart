1f552fd1f0c3061548106cc254a9d65add55c3e3
Abhijit Biswas <62851762+Abhijit-007a@users.noreply.github.com>
2021-06-30 15:12:30 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          fontFamily: "Nunito"
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+
+        ),
+      ),
+    );
+  }
+}
